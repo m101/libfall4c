2c6d6e0c76bc871a1de0488c4fec7c9b991a0f7b
m_101 <m101.sec@gmail.com>
2010-02-22 20:05:53 +0100

    Cleaned up some code, translate in English and deprecated some functions.\n

--- pathfinding.h
+++ pathfinding.h
@@ -9,16 +9,16 @@
 struct path_node
 {
     struct t_position Position;
-    // Noeud parent
+    // Parent node
     struct t_position ParentNodePoint;
     struct List *ParentNodestruct List;
     long type;
-    long poids;
+    long weigth;
     long direction;
     // Manhattan : F = G + H
-    // F : Cout estimer
-    // G : Cout a partir du point de depart
-    // H : Cout "heuristique" jusqu'a l'arrivee
+    // F : Estimated cost
+    // G : Cost from starting position
+    // H : Cost heuristic to arrival
     long cost;                   // F
     long costFromStart;          // G
     long costToGoal_heuristic;   // H
@@ -28,21 +28,33 @@ struct path_node
 
 /* A Star : Pathfinding
 */
-struct List* path_find_astar (struct t_position Depart, struct t_position Arrivee); //, t_unit Unite);
-long path_calcul_manhattan_heuristic (struct t_position Depart, struct t_position Arrivee); //, t_unit Unite);
+// pathfinding : A*
+struct List* path_find_astar (struct t_position Start, struct t_position Arrival);
+
+// pathfinding heuristic : manhattan
+long path_heuristic_manhattan (struct t_position Start, struct t_position Arrival);
+
+// find the lowest cost
 struct List* path_find_lowestcost (struct List *Noeud);
-// Ajouter un noeud a la struct Liste chainee
-struct List* path_ajouter_noeud_struct Liste (struct List *List, struct List *Appendedstruct Liste);
-// Enlever un noeud a la struct Liste chainee
-struct List* path_enlever_noeud_struct Liste (struct List *List, struct List *Appendedstruct Liste);
-// "Stocker" un t_path_node dans la struct Liste
-void path_mov_node_to_struct List (struct List *List, t_path_node Node);
-// "Stocker" une struct Liste dans un t_path_node
-void path_mov_struct List_to_node (t_path_node *Node, struct List *List);
-// Trouve un noeud selon sa position (x, y)
-struct List* path_find_noeud (struct List *List, struct t_position Position);
-// Tri de la struct Liste par cout
-struct List* path_tri_struct Liste (struct List *List);
+
+// append a node to path
+int path_list_node_add (struct List *List, struct List *Node);
+
+// remove a node from path
+int path_list_node_delete (struct List *Node);
+
+// convert a path node to a list
+struct List* path_node_to_list (struct t_path_node *Node)
+
+// convert a list to a path node
+struct t_path_node* path_list_to_node (struct List *List)
+
+// find a node according to it's position
+struct List* path_find_node (struct List *List, struct t_position Position);
+
+// sort path nodes according to cost
+struct List* path_sort_list (struct List *List);
+
 // Initialisation d'un t_path_node
 void path_node_zero (t_path_node *Node);
 
