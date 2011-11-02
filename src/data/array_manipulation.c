c94502bcae2226f1ae816ce6722e449e09397b8f
m_101 <m101.sec@gmail.com>
2011-11-02 23:37:35 +0000

    - Mod(libdata): Cleaned array_manipulation (useless code)

--- src/data/array_manipulation.c
+++ src/data/array_manipulation.c
@@ -5,16 +5,6 @@
 
 #include "array_manipulation.h"
 
-void printArray (long array[], size_t i)
-{
-    size_t j = 0;
-
-    for (j = 0; j < i; j++)
-    {
-        printf("%ld\n", array[j]);
-    }
-}
-
 long* num2array (long number, long *array, size_t *i)
 {
     long i_temp = 0, j = 0, temp1 = 0;
@@ -46,127 +36,28 @@ long* num2array (long number, long *array, size_t *i)
     return array;
 }
 
-long* numString2array (char *number, long *array, size_t *i)
-{
-    long numberLength = strlen(number);
-    *i = numberLength;
-    long *longArray = malloc (sizeof(*longArray) * numberLength + 1);
-    long *pArray = longArray;
-    //long j = numberLength - 1;
-
-    while (*number != '\0')
-    {
-        //*(longArray + j) = *number - '0';
-        *longArray = *number - '0';
-        longArray++;
-        number++;
-        //j--;
-    }
-
-    return pArray;
-}
-
-long numCount (long number)
-{
-    long count = 0;
-
-    do
-    {
-        number /= 10;
-        count++;
-    }
-    while (number > 0);
-
-    return count;
-}
-
-long countDigitsInNumber (long digits)
-{
-    long i = 0;
-
-    do
-    {
-        digits /= 10;
-        i++;
-    } while (digits > 0);
-
-    return i;
-}
-
-void ArrayInit (long tableau[])
-{
-    while (*tableau)
-    {
-        *tableau = 0;
-        tableau++;
-    }
-}
-
-//Somme de toutes les valeurs d'un tableau
-long sommeTableau(long tableau[], long tailleTableau)
-{
-    long somme = 0, i;
-
-    for (i = 0; i < tailleTableau; i++)
-    {
-        somme += tableau[i];
-    }
-
-    return somme;
-}
-
-//Moyenne de la somme des valeurs contenues dans un tableau.
-double moyenneTableau(long tableau[], long tailleTableau)
-{
-    double moyenne = 0, somme;
-
-    somme = sommeTableau(tableau, tailleTableau);
-    moyenne = somme/tailleTableau;
-
-    return moyenne;
-}
-
-void copie(long tableauOriginal[], long tableauCopie[], long tailleTableau)
-{
-    int i;
-
-    //Fait une copie conforme tu tableau original
-    for (i=0; i < tailleTableau; i++)
-    {
-        tableauCopie[i] = tableauOriginal[i];
-    }
-}
-
-void maximumTableau(long tableau[], long tailleTableau, long valeurMax)
-{
-    int i;
-
-    for (i = 0; i < tailleTableau; i++)
-    {
-        //Si la valeur d'un element du tableau est superieur a valeur max alors
-        //celui-ci est re-initialiser a 0
-        if (tableau[i] > valeurMax)
-        {
-            tableau[i] = 0;
-        }
-    }
-}
-
-void ordonnerTableau(long tableau[], long tailleTableau)
-{
-    long temp = 0;
+// generic bubble sort
+void sort_bubble (void *array, size_t nmemb, size_t size, int(*compar)(const void *, const void *)) {
+    void *temp = NULL;
     int i, j;
+    int check;
 
-    for (i = 0; i < (tailleTableau-1); i++)
-    {
-        for (j=i++; j < tailleTableau; j++)
-        {
-            if (tableau[i] > tableau[j])
-            {
-                temp = tableau[i];
-                tableau[i] = tableau[j];
-                tableau[j] = temp;
+    // alloc temp
+    temp = calloc(1, size);
+    if (!temp)
+        return;
+
+    for (i = 0; i < (nmemb-1); i++) {
+        for (j=i++; j < nmemb; j++) {
+            check = compar(array+i, array+j);
+            if (check > 0) {
+                memcpy(temp, array+i, size);
+                memcpy(array+i, array+j, size);
+                memcpy(array+j, temp, size);
             }
         }
     }
+
+    free(temp);
 }
+
