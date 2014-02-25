1e2aaa5b2b28edf5c3f62dd83210c810e0cb6fa7
m_101 <m101.sec@gmail.com>
2014-02-25 15:36:41 +0100

    Add: GPL headers missing in some files

--- inc/data/cache.h
+++ inc/data/cache.h
@@ -1,3 +1,20 @@
+/*
+    This file is part of fall4c.
+
+    fall4c is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    fall4c is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with fall4c.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _CACHE_H_
 #define _CACHE_H_
 
